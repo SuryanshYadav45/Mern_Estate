07df9e1fc0ac6bb12740a1787965a5509e2b9dd3
Suryansh Yadav <suryanshyadav159@gmail.com>
2023-12-26 17:58:49 +0530

    third commit

--- api/index.js
+++ api/index.js
@@ -5,6 +5,10 @@ app.get("/",(req,res)=>
 {
 res.send("this is from the server")
 })
+app.post("/signin",async(req,res)=>
+{
+    res.status(200).json("this is signin api")
+})
 
 app.listen(4000,()=>
 {
