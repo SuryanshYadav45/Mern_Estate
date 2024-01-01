889bf86735631c25dd3792be7e443022fbc142fe
Suryansh Yadav <suryanshyadav159@gmail.com>
2024-01-01 19:29:53 +0530

    profile page UI changed

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { useSelector } from 'react-redux'
 import { jwtDecode } from "jwt-decode";
 
@@ -7,11 +7,14 @@ const Profile = () => {
   const { currentuser } = useSelector((state) => state.user)
   const decoded = currentuser ? jwtDecode(currentuser.token) : null
   const { photoUrl, username, email } = decoded || {};
+  const fileref=useRef(null);
+
   return (
     <div className='w-full h-[calc(100vh-72px)] bg-gray-200 p-2'>
       <div className='max-w-[600px]  m-auto '>
         <h1 className='capitalize text-center font-semibold text-[25px] tabl:text-[40px] my-4'>profile</h1>
-        <img src={photoUrl} className='mx-auto my-3 rounded-full w-[80px] lg:w-[140px] tabl:w-[120px]' alt="" />
+        <input type="file" hidden ref={fileref} accept='image/*'/>
+        <img src={photoUrl} onClick={()=>fileref.current.click()} className='mx-auto my-3 rounded-full w-[80px] lg:w-[140px] tabl:w-[120px] cursor-pointer' alt="" />
         <form className='flex justify-center flex-col items-center'>
           <input type="text" className=' w-full text-[14px] my-3 h-10 p-2 outline-2 border outline-none border-gray-500 focus:border-gray-900 focus:border-2 rounded-lg moblg:w-[380px] tabl:w-[500px] lg:w-[500px] moblg:text-[18px]' placeholder='username' />
             
@@ -19,7 +22,7 @@ const Profile = () => {
           <button className='bg-[#2f5d56] w-full moblg:w-[380px] tabl:w-[500px] lg:w-[500px] text-white p-2 rounded-md uppercase' >Update</button>
           <button className='bg-[#369434] w-full moblg:w-[380px] tabl:w-[500px] lg:w-[500px] text-white p-2 my-4 rounded-md uppercase' >Create Property</button>
         </form>
-        <div className='flex justify-between items-center'>
+        <div className='flex justify-between items-center  moblg:w-[380px] tabl:w-[500px] lg:w-[500px] m-auto'>
           <p className='text-red-700 capitalize'>delete account</p>
           <p className='text-red-700 capitalize'>sign out</p>
         </div>
