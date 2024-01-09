4aadd037f279f7ff05d1ff58f65b30fc05d8d448
Suryansh Yadav <suryanshyadav159@gmail.com>
2024-01-09 21:18:02 +0530

    created delete listing api

--- api/controller/listingController.js
+++ api/controller/listingController.js
@@ -30,11 +30,33 @@ const userListing = async (req, res) => {
         res.status(500).json("error occured")
     }
 }
+const deleteListing = async (req, res) => {
+    try {
+        const{userid}=req.body;
+        const property= await PropertyModel.findById(req.params.id)
+        if(!property)
+        {
+            res.status(404).json("no property found")
+        }
+
+        if(userid!=property.userid){
+            res.status(403).json("You can only delete the listing created by you")
+        }
+        await PropertyModel.findByIdAndDelete(req.params.id);
+        res.status(200).json("property deleted successfully");
+
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json("error occured")
+    }
+}
 
 
 
 module.exports = {
     createListing,
     getListing,
-    userListing
+    userListing,
+    deleteListing
 }
--- api/router/listingRouter.js
+++ api/router/listingRouter.js
@@ -1,9 +1,10 @@
 const express= require("express");
-const {createListing,getListing, userListing}=require("../controller/listingController.js")
+const {createListing,getListing, userListing, deleteListing}=require("../controller/listingController.js")
 const router=express.Router();
 
 router.post('/createlisting',createListing)
 router.get('/getlisting',getListing)
 router.get('/userlisting/:id',userListing)
+router.delete('/deletelisting/:id',deleteListing)
 
 module.exports=router;
