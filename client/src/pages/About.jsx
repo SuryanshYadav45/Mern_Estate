c40b5ea638f10eb047ecee0c039b6c3244f730ba
Suryansh Yadav <suryanshyadav159@gmail.com>
2024-01-21 15:23:17 +0530

    added the styling to about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const About = () => {
   return (
     <div className='w-full  min-h-[calc(100vh-72px)] bg-[rgba(72,71,71,0.1)]'>
-      <div className='max-w-[1150px] m-auto'>
-        <h1 className=''>About Realtor</h1>
-        <p>RealTor is a leading real estate agency that specializes in helping clients buy, sell, and rent properties in the most desirable neighborhoods. Our team of experienced agents is dedicated to providing exceptional service and making the buying and selling process as smooth as possible.</p>
-        <p>Our mission is to help our clients achieve their real estate goals by providing expert advice, personalized service, and a deep understanding of the local market. Whether you are looking to buy, sell, or rent a property, we are here to help you every step of the way.</p>
-        <p>Our team of agents has a wealth of experience and knowledge in the real estate industry, and we are committed to providing the highest level of service to our clients. We believe that buying or selling a property should be an exciting and rewarding experience, and we are dedicated to making that a reality for each and every one of our clients.</p>
-        <p>Our vision is to revolutionize the way people buy, sell, and explore real estate. We aim to be the go-to destination for individuals and families seeking their dream homes, offering a platform that combines cutting-edge technology with personalized service.</p>
+      <div className='max-w-[1150px] m-auto p-2'>
+        <h1 className='font-semibold text-[23px] text-[#1b5051] underline tabl:text-[35px]'>About RealTor</h1>
+        <p className='text-justify py-2 text-[rgba(0,0,0,0.8)] tabl:text-[20px]'>RealTor is a leading real estate agency that specializes in helping clients buy, sell, and rent properties in the most desirable neighborhoods. Our team of experienced agents is dedicated to providing exceptional service and making the buying and selling process as smooth as possible.</p>
+        <p className='text-justify py-2 text-[rgba(0,0,0,0.8)]  tabl:text-[20px]'>Our mission is to help our clients achieve their real estate goals by providing expert advice, personalized service, and a deep understanding of the local market. Whether you are looking to buy, sell, or rent a property, we are here to help you every step of the way.</p>
+        <p className='text-justify py-2 text-[rgba(0,0,0,0.8)]  tabl:text-[20px]'>Our team of agents has a wealth of experience and knowledge in the real estate industry, and we are committed to providing the highest level of service to our clients. We believe that buying or selling a property should be an exciting and rewarding experience, and we are dedicated to making that a reality for each and every one of our clients.</p>
+        <p className='text-justify py-2 text-[rgba(0,0,0,0.8)]  tabl:text-[20px]'>Our vision is to revolutionize the way people buy, sell, and explore real estate. We aim to be the go-to destination for individuals and families seeking their dream homes, offering a platform that combines cutting-edge technology with personalized service.</p>
       </div>  
     </div>
   )
