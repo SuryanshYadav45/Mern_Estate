9e1fe9b2041e1af5b6579149d083e059f55edbbd
Suryansh Yadav <suryanshyadav159@gmail.com>
2024-01-23 21:50:07 +0530

    completed the ui for the order  succes page

--- client/src/pages/Success.jsx
+++ client/src/pages/Success.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import success from "../assets/orderSuccess.gif"
+import { useNavigate } from 'react-router-dom'
 const Success = () => {
+  const navigate=useNavigate();
   return (
-    <div className='w-full h-[calc(100vh-72px)] flex justify-center items-center'>
-        
+    <div className='w-full h-[calc(100vh-72px)] flex justify-center '>
+        <div className='mt-[100px]'>
+        <img src={success} className='w-[250px]' alt="" />
+        <p className='text-center text-[30px] font-extrabold text-green-800'>Order Successful</p>
+        <button className='w-full mt-10 h-[45px] rounded-md bg-green-700 text-white' onClick={()=>navigate("/")}>Explore More Property</button>
+        </div>
     </div>
   )
 }
