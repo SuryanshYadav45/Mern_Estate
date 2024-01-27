77e9b12d3457ac04349766c313e303c84695e475
Suryansh Yadav <suryanshyadav159@gmail.com>
2024-01-27 19:28:19 +0530

    integrated the purchased api in the frontend

--- client/src/pages/Orders.jsx
+++ client/src/pages/Orders.jsx
@@ -1,6 +1,7 @@
 import { jwtDecode } from 'jwt-decode';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux';
+import UserListing from '../components/UserListing';
 
 function Orders() {
     const [property, setproperty] = useState([])
@@ -8,20 +9,22 @@ function Orders() {
     const decoded = currentuser ? jwtDecode(currentuser.token) : null
     const { id } = decoded || {};
 
-    // useEffect(()=>{
+    useEffect(()=>{
 
-    //     const fetchdata=async()=>{
-    //         const res= await fetch(`http://localhost:4000/listing/userlisting/${id}`);
-    //         const data= await res.json();
-    //         setproperty(data);
-    //     }
-    //     fetchdata();
+        const fetchdata=async()=>{
+            const res= await fetch(`http://localhost:4000/listing/purchased/${id}`);
+            const data= await res.json();
+            setproperty(data);
+        }
+        fetchdata();
 
-    // },[])
+    },[])
 
+    console.log(property);
   return (
     <div className='w-full '>
        
+this page will soon display the property bought by you 
         {
             property?.map((data)=>
             {
