75ef726d62f434160628e1ad3d78a607448d8a4d
Suryansh Yadav <suryanshyadav159@gmail.com>
2024-01-29 23:28:18 +0530

    updated the update user api

--- api/controller/userController.js
+++ api/controller/userController.js
@@ -10,20 +10,24 @@ res.status(200).send("getuser api is working")
 
 const updateUser=async(req,res)=>{
 
-    const{username,email,photoUrl}=req.body;
-
+    const{username,email,photourl}=req.body;
     const user= await UserModel.findOne({_id:req.params.id})
     if(!user){
         res.status(404).send("user not found")
     }
     else{
         user.username=username?username:user.username;
-        user.photoUrl=photoUrl?photoUrl:user.photoUrl;
+        user.photoUrl=photourl?photourl:user.photoUrl;
         user.email=email?email:user.email;
 
         const updateduser= await user.save();
+        if(updateUser){
         const token= jwt.sign({ id: user._id, username: user.username ,photoUrl:user.photoUrl,email:user.email }, process.env.TOKEN_SECRET, { expiresIn: '1h' });
         res.status(200).json({token});
+        }
+        else{
+        res.status(500).json("Server Error");
+        }
     }
 
 
